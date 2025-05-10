2011902d0cdea3a102241de6007677a19b3d290c
Ludwig S <silvaludwig@outlook.com>
2025-05-10 17:00:22 -0300

    changing plans... making an API  instead | JWT token

--- mysocial/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('terms/', views.terms, name='terms'),
-    path('about/', views.about, name='about'),
-]
